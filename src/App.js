e2c705e19799bcff4d15ed0a9af229363b2e3ec1
Phillip Kang <phillipxkang@gmail.com>
2019-04-15 23:58:48 -0700

    A journey of a thousand miles starts with a single step

--- src/App.js
+++ src/App.js
@@ -6,9 +6,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <p>
-            GOOD CONTENT STUDIO
-          </p>
+
+        <div class="outer">
+          <div class="middle">
+
+            <div class="inner">
+              <a href="https://instagram.com/goodcontentstudio">
+                <h1 class="name">
+                  <h1 class="insta">@</h1>
+                    GOOD
+                    <h1 class="nospace"> </h1>
+                    CONTENT
+                    <h1 class="nospace"> </h1>
+                    STUDIO
+                </h1>
+              </a>
+            </div>
+            <div className="pyro">
+              <div className="before"></div>
+              <div claclassNamess="after"></div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
